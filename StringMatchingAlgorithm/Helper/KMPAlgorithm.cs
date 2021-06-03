57269b89e8016a7e8a21c5d3d794c36ade1a6d06
Rahul Kumar <rahul.kumar14@stryker.com>
2021-06-03 21:22:51 +0530

    Add project files.

---
+++ StringMatchingAlgorithm/Helper/KMPAlgorithm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMatchingAlgorithm.Helper
+{
+    internal class KMPAlgorithm
+    {
+        internal void MatchString()
+        {
+            Console.Write("Enter the string : ");
+            string mainString = Console.ReadLine();
+
+            Console.Write("Enter the string : ");
+            string pattern = Console.ReadLine();
+
+            int[] pie = new int[pattern.Length];
+            int i = 0, j = 0;
+
+            ComputeLPSArray(pattern, pattern.Length, pie);
+
+            while (i < mainString.Length && j < pattern.Length)
+            {
+                if (pattern[j] == mainString[i])
+                {
+                    j++;
+                    i++;
+                }
+                else
+                {
+                    if (j != 0)
+                        j = pie[j - 1];
+                    else
+                        i++;
+                }
+            }
+
+            if (j == pattern.Length)
+            {
+                Console.WriteLine($"Found pattern at index : {i - j}");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not found");
+            }
+        }
+
+        private void ComputeLPSArray(string patter, int M, int[] lps)
+        {
+            int len = 0, i = 1;
+            lps[0] = 0;
+
+            while (i < patter.Length)
+            {
+                if (patter[i] == patter[len])
+                {
+                    len++;
+                    lps[i] = len;
+                    i++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        len = lps[len - 1];
+                    }
+                    else
+                    {
+                        lps[i] = len;
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StringMatchingAlgorithm/Program.cs
@@ -0,0 +1,45 @@
+﻿using StringMatchingAlgorithm.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMatchingAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine
+                    (Environment.NewLine + "Please select an option" +
+                        Environment.NewLine + "1. String Matching(KMP)" +
+                        Environment.NewLine + "0. Exit"
+                    );
+
+                if (!int.TryParse(Console.ReadLine(), out int i))
+                {
+                    Console.WriteLine(Environment.NewLine + "Input format is not valid. Please try again." + Environment.NewLine);
+                }
+
+                if (i == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else if (i == 1)
+                {
+                    KMPAlgorithm kMPAlgorithm = new KMPAlgorithm(); //ababcabcabababd, ababd
+                    kMPAlgorithm.MatchString();
+                }
+                else
+                {
+                    Console.WriteLine("Please select a valid option.");
+                }
+
+
+            }
+        }
+    }
+}
